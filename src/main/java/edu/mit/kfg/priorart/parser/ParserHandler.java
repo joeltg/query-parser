f270e1331fc4d761c3a443fc22b7716588ef78dd
Justin Reese <justin@justinreese.com>
2019-07-29 18:34:55 -0500

    Take a stab at logging query to CloudWatch
    This is my attempt at logging the `request.query` to CloudWatch.
    
    I’m not sure this is the correct way, and intended to just build, YOLO,
    and upload, but I can’t get Lambda to take my built JAR so I’m
    committing this possibly wrong code instead.
    
    Affects #3

--- src/main/java/edu/mit/kfg/priorart/parser/ParserHandler.java
+++ src/main/java/edu/mit/kfg/priorart/parser/ParserHandler.java
@@ -2,11 +2,13 @@ package edu.mit.kfg.priorart.parser;
 
 import com.uspto.query.parser.QueryParser;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class ParserHandler implements RequestHandler<RequestClass, String> {
 	public String handleRequest(RequestClass request, Context context) {
 		QueryParser queryParser = new QueryParser();
+		System.out.println("request.query was: " + request.query);
 		return queryParser.pasrseToElastic(request.query, request.operator, request.filters);
 	}
 }
